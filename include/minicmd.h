f2dc57719c4c09aab154a6fc7cc6fb253d43451d
Louis Girerd <lgirerd@z4r6p2.42lyon.fr>
2025-03-27 17:28:07 +0100

    add : execute demo

---
+++ include/minicmd.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   louis.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 16:38:05 by lgirerd           #+#    #+#             */
+/*   Updated: 2025/03/27 16:46:22 by lgirerd          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LOUIS_H
+# define LOUIS_H
+
+# include <readline/readline.h>
+
+# define CMD_NOT_FOUND 127
+# define EXEC_FAIL 126
+
+#endif
