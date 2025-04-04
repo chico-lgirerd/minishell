31b491a95c7b37951c81738b7f01cecaac12ac1c
Louis Girerd <lgirerd@z4r5p2.42lyon.fr>
2025-04-04 12:57:15 +0200

    add : added 1 more free

--- srcs/minicmd.c
+++ srcs/minicmd.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 16:13:20 by lgirerd           #+#    #+#             */
-/*   Updated: 2025/04/04 12:20:02 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2025/04/04 12:33:57 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,7 +103,10 @@ int	main(int ac, char **av, char **envp)
 	{
 		input = readline("cmd-demo> ");
 		if (!input)
+		{
+			free(envp);
 			break ;
+		}
 		else if (ft_strncmp(input, "echo ", 5) == 0)
 			ft_echo(input, envp);
 		else if (ft_strncmp(input, "cd ", 3) == 0)
