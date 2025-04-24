87d3142da05eeb36dbecfe39fcff84ac9229276e
Louis Girerd <lgirerd@z4r9p5.42lyon.fr>
2025-04-24 15:07:13 +0200

    fix : updated pwd to implement g_exit_value

--- srcs/pwd.c
+++ srcs/pwd.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 17:15:53 by lgirerd           #+#    #+#             */
-/*   Updated: 2025/04/02 17:18:42 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2025/04/24 15:06:15 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	pwd(void)
+int	pwd(void)
 {
 	char	cwd[1024];
 
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
-		output_error(errno);
+		return (output_error(errno));
 	else
 		printf("%s\n", cwd);
+	return (0);
 }
