65e992d18efff0b9f37b909982330e152d5ef829
Louis Girerd <lgirerd@z4r11p6.42lyon.fr>
2025-04-29 18:11:47 +0200

    add : cd no arguments goes to home

--- srcs/cd.c
+++ srcs/cd.c
@@ -6,32 +6,51 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 14:37:55 by lgirerd           #+#    #+#             */
-/*   Updated: 2025/04/29 16:16:18 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2025/04/29 18:11:31 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "errors.h"
+#include "colors.h"
 #include <dirent.h>
 #include <unistd.h>
-#include "stdio.h"
+#include <stdio.h>
+
+char	*get_dirpath(char **args)
+{
+	char	*dirpath;
+	
+	if (!args[0])
+	{
+		dirpath = getenv("HOME");
+		if (!dirpath)
+		{
+			printf(RED"minishell: cd: HOME not set\n"RESET);
+			return (1);
+		}
+	}
+	else
+		dirpath = args[0];
+	while (*dirpath == ' ' || *dirpath == '\t')
+		dirpath++;
+	if (*dirpath == '\0')
+		return (output_error(ENOENT));
+	return (dirpath)
+}
 
 int	cd(char **args)
 {
 	DIR		*dir;
 	char	*dirpath;
-
-	if (!args[0])
-		return (1);
+	
 	if (args[1])
 	{
 		printf("minishell: cd: too many arguments\n");
 		return (1);
 	}
-	dirpath = args[0];
-	while (*dirpath == ' ' || *dirpath == '\t')
-		dirpath++;
-	if (*dirpath == '\0')
-		return (output_error(ENOENT));
+	dirpath = get_dirpath(args);
+	if (!dirpath)
+		return (1);
 	dir = opendir(dirpath);
 	if (dir)
 	{
