db3b812ef1818f43990d96593ffbb1cafb56a934
Louis Girerd <lgirerd@z4r5p1.42lyon.fr>
2025-05-01 13:45:03 +0200

    fix : fixed cd without arguments -> HOME

--- srcs/cd.c
+++ srcs/cd.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 14:37:55 by lgirerd           #+#    #+#             */
-/*   Updated: 2025/04/29 18:11:31 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2025/05/01 13:44:40 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char	*get_dirpath(char **args)
 {
 	char	*dirpath;
-	
+
 	if (!args[0])
 	{
 		dirpath = getenv("HOME");
 		if (!dirpath)
 		{
 			printf(RED"minishell: cd: HOME not set\n"RESET);
-			return (1);
+			return (NULL);
 		}
 	}
 	else
@@ -34,30 +35,23 @@ char	*get_dirpath(char **args)
 	while (*dirpath == ' ' || *dirpath == '\t')
 		dirpath++;
 	if (*dirpath == '\0')
-		return (output_error(ENOENT));
-	return (dirpath)
+		return (NULL);
+	return (dirpath);
 }
 
 int	cd(char **args)
 {
-	DIR		*dir;
 	char	*dirpath;
-	
-	if (args[1])
-	{
-		printf("minishell: cd: too many arguments\n");
-		return (1);
-	}
+
 	dirpath = get_dirpath(args);
 	if (!dirpath)
 		return (1);
-	dir = opendir(dirpath);
-	if (dir)
+	if (args[0] && args[1])
 	{
-		closedir(dir);
-		if (chdir(dirpath) != 0)
-			return (output_error(errno));
-		return (0);
+		printf(RED"minishell: cd: too many arguments\n"RESET);
+		return (1);
 	}
-	return (output_error(errno));
+	if (chdir(dirpath) != 0)
+		return (output_error(errno));
+	return (0);
 }
