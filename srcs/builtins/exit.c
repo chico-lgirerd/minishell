d271802e172a72857dbe45a172087bade0f48678
Louis Girerd <lgirerd@z4r3p7.42lyon.fr>
2025-06-12 18:24:30 +0200

    fix : changed if order in exit

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 16:34:07 by lgirerd           #+#    #+#             */
-/*   Updated: 2025/06/06 09:53:38 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2025/06/12 18:24:09 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,13 @@ int	ft_exit(char **args, t_data *data)
 		free_all_data(data, true);
 		exit(data->exit_value);
 	}
+	if (!is_numeric(args[0]))
+		exit_non_numeric(data, args[0]);
 	if (args[1])
 	{
 		ft_putendl_fd(RED"minishell: exit: too many arguments"RESET, 2);
 		return (1);
 	}
-	if (!is_numeric(args[0]))
-		exit_non_numeric(data, args[0]);
 	exitcode = ft_atoll(args[0]);
 	if ((exitcode == -1 || exitcode == 0) && (ft_strcmp(args[0], "-1")
 			!= 0 && ft_strcmp(args[0], "0")))
